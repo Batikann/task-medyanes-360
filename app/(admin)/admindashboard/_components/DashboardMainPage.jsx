f55a6f7a419f23c3ce3d5c04115d167ed5fb9581
Batikann <emiruar123@gmail.com>
2024-06-11 12:03:08 +0300

    change admin pages

---
+++ app/(admin)/admindashboard/_components/DashboardMainPage.jsx
@@ -0,0 +1,130 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import { getAPI } from '../../../../services/fetchAPI'
+
+import { useRouter, useSearchParams } from 'next/navigation'
+import { normalizeInput } from '../../../../lib/utils/formatter'
+import SearchBar from '../../../../components/Searchbar/index'
+import DashboardCard from '../../../../components/DashboardCard/index'
+import AdminDashboardTable from '../../../../components/AdminDashboardTable/index'
+
+const DashboardMainPage = () => {
+  const [completedTaskCount, setCompletedTaskCount] = useState()
+  const [tasks, setTasks] = useState([])
+  const [inputValue, setInputValue] = useState('')
+  const [statusType, setStatusType] = useState('ALL')
+  const [inProgressTaskCount, setInProgressTaskCount] = useState()
+  const [allTaskCount, setAllTaskCount] = useState()
+  const [loading, setLoading] = useState(true)
+  const router = useRouter() // Router nesnesi
+  const searchParams = useSearchParams()
+
+  useEffect(() => {
+    //Veritabanından tüm tasklerin geçerli durumlara göre sayısını getiren fonksiyon
+    const getTasks = async () => {
+      try {
+        const taskParam = statusType // task parametresini alır, yoksa 'ALL' kullanır
+        const queryParam = searchParams.get('query') || '' // query parametresini alır, yoksa boş string kullanır
+        setLoading(true)
+        const taskData = await getAPI(`/tasks/${taskParam}/get-tasks`)
+        if (taskData.status === 'success') {
+          const { tasks } = taskData.data
+
+          const filteredTasks = queryParam
+            ? tasks.filter(
+                (task) =>
+                  normalizeInput(task?.title)
+                    .toLowerCase()
+                    .includes(queryParam.toLowerCase()) || // Başlıkta arama yapar
+                  task.assignedUsers.some(
+                    (user) =>
+                      normalizeInput(user.user?.name)
+                        .toLowerCase()
+                        .includes(queryParam.toLowerCase()) // Kullanıcı adında arama yapar
+                  )
+              )
+            : tasks // Filtrelenmiş task listesini oluşturur
+
+          setTasks(filteredTasks) // Task listesini günceller
+          setLoading(false) // Yüklenme durumunu false yapar
+        } else {
+          // Hata varsa console ekranında göstericek
+          console.error('Failed to fetch task counts')
+        }
+      } catch (error) {
+        // Ağ hatasını kontrol etmek için
+        console.error('Error while fetching task counts:', error)
+      }
+    }
+
+    getTasks()
+  }, [searchParams])
+
+  useEffect(() => {
+    const getDashboardCount = async () => {
+      const dashboard = await getAPI('/tasks/get-task-dashboard')
+      setCompletedTaskCount(dashboard.completedTaskCount)
+      setInProgressTaskCount(dashboard.inProgressTaskCount)
+      setAllTaskCount(dashboard.allTaskCount)
+    }
+    getDashboardCount()
+  }, [])
+
+  useEffect(() => {
+    const handleURLUpdate = () => {
+      const taskParam = normalizeInput(statusType) // task parametresini alır, yoksa 'ALL' kullanır
+      const normalizedQuery = normalizeInput(inputValue) // Giriş değerini normalize eder
+      if (normalizedQuery) {
+        router.push(
+          `/admindashboard?task=${taskParam}&query=${normalizedQuery}`,
+          undefined,
+          { shallow: true }
+        ) // URL'yi query ile günceller
+      } else {
+        router.push(`/admindashboard?task=${taskParam}`, undefined, {
+          shallow: true,
+        }) // URL'yi query olmadan günceller
+      }
+    }
+
+    const timer = setTimeout(handleURLUpdate, 500) // 500ms sonra URL'yi günceller
+
+    return () => clearTimeout(timer) // Temizleme fonksiyonu
+  }, [inputValue, router, searchParams, statusType])
+
+  return (
+    <div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+        <DashboardCard
+          bgColor={'bg-[#DBDFEA]'}
+          textColor={'text-[#8294C4]'}
+          title={'Toplam Görev'}
+          count={allTaskCount}
+        />
+        <DashboardCard
+          title={'Tamamlanan Görev'}
+          count={completedTaskCount}
+          bgColor={'bg-[#D0E7D2]'}
+          textColor={'text-[#618264]'}
+        />
+        <DashboardCard
+          title={'Devam Eden Görev'}
+          bgColor={'bg-[#FEECE2]'}
+          textColor={'text-[#FFBE98]'}
+          count={inProgressTaskCount}
+        />
+      </div>
+      <div className="mt-4">
+        <SearchBar inputValue={inputValue} setInputValue={setInputValue} />
+      </div>
+      <AdminDashboardTable
+        tasks={tasks}
+        loading={loading}
+        setStatusType={setStatusType}
+        statusType={statusType}
+      />
+    </div>
+  )
+}
+export default DashboardMainPage
--- app/(admin)/admindashboard/task/page.jsx
+++ app/(admin)/admindashboard/task/page.jsx
@@ -1,95 +1,11 @@
-'use client'
-
-import { Suspense, useEffect, useState } from 'react'
-import { useRouter, useSearchParams } from 'next/navigation'
-import { getAPI } from '../../../../services/fetchAPI/index.js'
-import TaskList from '../../../../components/TaskList/index.jsx'
-import { normalizeInput } from '../../../../lib/utils/formatter.js'
-import SearchBar from '../../../../components/Searchbar'
+import { Suspense } from 'react'
+import MainTaskPage from './_components/MainTaskPage'
 
 const TasksPage = () => {
-  const [tasks, setTasks] = useState([]) // Task verilerini tutar
-  const [loading, setLoading] = useState(true) // Yüklenme durumunu tutar
-
-  const [inputValue, setInputValue] = useState('') // Arama giriş değerini tutar
-
-  const router = useRouter() // Router nesnesi
-  const searchParams = useSearchParams() // URL parametrelerini alır
-
-  useEffect(() => {
-    const getTasks = async () => {
-      const taskParam = searchParams.get('task')?.toUpperCase() || 'ALL' // task parametresini alır, yoksa 'ALL' kullanır
-      const queryParam = searchParams.get('query') || '' // query parametresini alır, yoksa boş string kullanır
-      setLoading(true)
-
-      const taskData = await getAPI(`/tasks/${taskParam}/get-tasks`) // API'den task verilerini çeker
-      const allTasks = taskData.data.tasks // Gelen verilerden task listesini alır
-
-      const filteredTasks = queryParam
-        ? allTasks.filter(
-            (task) =>
-              normalizeInput(task?.title)
-                .toLowerCase()
-                .includes(queryParam.toLowerCase()) || // Başlıkta arama yapar
-              task.assignedUsers.some(
-                (user) =>
-                  normalizeInput(user.user?.username)
-                    .toLowerCase()
-                    .includes(queryParam.toLowerCase()) // Kullanıcı adında arama yapar
-              )
-          )
-        : allTasks // Filtrelenmiş task listesini oluşturur
-
-      setTasks(filteredTasks) // Task listesini günceller
-      setLoading(false) // Yüklenme durumunu false yapar
-    }
-    getTasks() // Taskları alır
-  }, [searchParams]) // searchParams değiştikçe çalışır
-
-  useEffect(() => {
-    const handleURLUpdate = () => {
-      const taskParam = searchParams.get('task') || 'ALL' // task parametresini alır, yoksa 'ALL' kullanır
-      const normalizedQuery = normalizeInput(inputValue) // Giriş değerini normalize eder
-      if (normalizedQuery) {
-        router.push(
-          `/admindashboard/task?task=${taskParam}&query=${normalizedQuery}`,
-          undefined,
-          { shallow: true }
-        ) // URL'yi query ile günceller
-      } else {
-        router.push(`/admindashboard/task?task=${taskParam}`, undefined, {
-          shallow: true,
-        }) // URL'yi query olmadan günceller
-      }
-    }
-
-    const timer = setTimeout(handleURLUpdate, 500) // 500ms sonra URL'yi günceller
-
-    return () => clearTimeout(timer) // Temizleme fonksiyonu
-  }, [inputValue, router, searchParams]) // inputValue, router veya searchParams değiştikçe çalışır
-
-  // Eğer Veritabanında hiç task yoksa kullanıcıyı bu konuda bilgilendiririz.
-  if (tasks.length <= 0 && !loading) {
-    return (
-      <div className="flex flex-col gap-5">
-        <SearchBar inputValue={inputValue} setInputValue={setInputValue} />
-        <p>Geçerli bir görev bulunamadı.</p>
-      </div>
-    )
-  }
-
   return (
-    <div className="flex flex-col gap-5">
-      <SearchBar inputValue={inputValue} setInputValue={setInputValue} />
-      {/* TaskList bileşenini render ediyoruz */}
-      <Suspense fallback={<p>loading..</p>}>
-        <TaskList
-          tasks={tasks}
-          loading={loading}
-          dashboard={'admindashboard'}
-        />
-      </Suspense>
-    </div>
+    <Suspense fallback={<p>loading...</p>}>
+      <MainTaskPage />
+    </Suspense>
   )
 }
 
